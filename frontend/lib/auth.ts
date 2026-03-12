0789321a1201aab5167f28b5f4e4f8a4d7a00e07
Abin Thomas <followgamer102@gmail.com>
2026-03-12 21:21:31 +0530

    feat: db + auth

---
+++ frontend/lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/index";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  trustedOrigins: ["*"],
+  baseURL: process.env.BETTER_AUTH_BASE_URL,
+  advanced: {
+    cookiePrefix: "overbranch",
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  sendOnSignUp: true,
+  autoSignInAfterVerification: true,
+  expiresIn: 3600,
+}
+);
